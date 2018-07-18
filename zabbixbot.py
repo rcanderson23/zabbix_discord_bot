30253f12f809708ebbb1e25080e11c9b29f4f494
Robert Anderson <rcanderson23@gmail.com>
2018-07-17 19:25:34 -0600

    function cleanup

--- zabbixbot.py
+++ zabbixbot.py
@@ -8,20 +8,17 @@ class zabbixbot:
     #Returns dict of host including item, last value, and unit
     def get_item(self, hostname, item):
         if item == 'all':
-            value = self.zab.item.get(host=hostname)
+            return self.zab.item.get(host=hostname)
         else:
-            value = self.zab.item.get(host=hostname, search={'key_': '%s' % item})
-        return value
+            return self.zab.item.get(host=hostname, search={'key_': '%s' % item})
 
     #Returns a list of host dicts including name and hostid
     def get_hosts(self):
-        hosts = self.zab.host.get(output=["hostid","name"])
-        return hosts
+        return self.zab.host.get(output=["hostid","name"])
 
     def get_problems(self):
-        problems = self.zab.trigger.get(only_true=1,
-                                        monitored=1,
-                                        active=1,
-                                        selectHosts=['host'],
-                                        expandDescription=1)
-        return problems
+        return self.zab.trigger.get(only_true=1,
+                                    monitored=1,
+                                    active=1,
+                                    selectHosts=['host'],
+                                    expandDescription=1)
